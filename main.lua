a3a875a86c2db4e2d7837ab3f3fe7e01a5099bee
Rodrigo235 <r.rodrigomoliveira@gmail.com>
2018-10-09 19:08:27 -0300

    texto

--- main.lua
+++ main.lua
@@ -6,7 +6,6 @@ local largura, altura = display.actualContentWidth, display.actualContentHeight
 tabuleiroView:constroiTabuleiro()
 
 texto:newTexto("titulo", "Batalha Naval", largura / 2, altura * 0.05, 40)
+texto:newTexto("navio", "Fragata", largura / 2, altura * 0.8, 30)
+texto:newTexto("info", "Adicione o Navio", largura / 2, altura * 0.7, 30)
 
-texto:newTexto("rodape", "Rodape", largura / 2, altura * 0.8, 40)
-
-texto:setTexto("titulo", "Tste")
--- view/textoView.lua
+++ view/textoView.lua
@@ -1,21 +1,30 @@
 local texto = {}
 
 function texto:newTexto(nome, mensagem, x, y, tamanhoFont)
-	texto.id = nome 
-	texto.texto = display.newText(mensagem, x, y, native.systemFontBold, tamanhoFont)
+	table.insert(texto, {id = nome, texto = display.newText(mensagem, x, y, native.systemFontBold, tamanhoFont)})
 end
 
 function texto:setTexto(nome, mensagem)
 	local var = texto:findByName(nome)
-	var.text = mensagem
+	var.texto.text = mensagem
 end
 
 function texto:findByName(nome)
 	for k,v in pairs(texto) do
-		if (v == nome) then
+		if (v.id == nome) then
 			return v
 		end
 	end
+end
+
+function texto:listName()
+	local names = {}
+	for k,v in pairs(texto) do
+		if(tonumber(k) ~= nil and tonumber(k) <= #self) then
+			names[k] = v.id
+		end
+	end
+	return names
 end
 
 return texto
